a466fb7f4d4ae05d50408ee328e41b8bf466925f
Rayhan Tsaury <86096057+rayhantsaury@users.noreply.github.com>
2023-12-18 23:14:34 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 import UserRoute from "./routes/UserRoute.js";
 
 const app = express();
-mongoose.connect("mongodb://localhost:27017/player", {
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
